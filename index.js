361678f4aae6b8a882bf4e26bc4bf12a8f282cc4
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-09-30 19:30:22 +0530

    index.js file updated

--- index.js
+++ index.js
@@ -11,10 +11,19 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+//route
+app.use("/api/v1", apiRoute)
 
 //error handling middleware
 app.use(notFound);
@@ -22,8 +31,6 @@ app.use(errorHandler);
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
-//route
-app.use('/api/v1', apiRoute);
 
 const PORT = process.env.PORT || 4000;
 
